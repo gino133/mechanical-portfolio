7b1f7c7f3133b2b4ccf015885f599dd68ef2bee4
gino133 <79187467+gino133@users.noreply.github.com>
2026-06-02 16:44:42 +0700

    Add files via upload

---
+++ backend/src/routes/productRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const { protect, admin } = require('../middleware/authMiddleware');
+const {
+    getProducts,
+    getFeaturedProducts,
+    getProductById,
+    createProduct,
+    updateProduct,
+    deleteProduct
+} = require('../controllers/productController');
+
+router.route('/')
+    .get(getProducts)
+    .post(protect, admin, createProduct);
+
+router.get('/featured', getFeaturedProducts);
+
+router.route('/:id')
+    .get(getProductById)
+    .put(protect, admin, updateProduct)
+    .delete(protect, admin, deleteProduct);
+
+module.exports = router;
