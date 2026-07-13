cb7d9acfee86b92b6f3d370e3741e8a57d6d53a5
gino133 <79187467+gino133@users.noreply.github.com>
2026-07-13 14:00:27 +0700

    Delete backend/src/routes/setupRoutes.js

--- backend/src/routes/setupRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createAdminViaUrl } = require('../controllers/setupController');
-
-router.get('/create-admin', createAdminViaUrl);
-
-module.exports = router;
